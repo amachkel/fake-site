4387c116a607d44bf7dfd71c0b61ca5137edcb52
amachkel <amachkel@gmail.com>
2022-07-27 18:03:17 -0400

    add bootstrap dependencies and edit hero image

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "09692044941593ed"; }
+/******/ 		__webpack_require__.h = function() { return "8916adf38a4120bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import heroImage from "../src/assets/images/hero-image.png";
-import Image from "next/image";
-
+// import heroImage from "../src/assets/images/hero-image.png";
+// import Image from "next/image";
 export default function Homepage() {
   return (
-    <div>
-      <Image src={heroImage} alt="Man flying a kite on the beach" />
+    <div className="jumbotron bg-cover jumbotron-fluid">
+      {/* <Image
+        className="heroImage container-fluid"
+        src={heroImage}
+        alt="Man flying a kite on the beach"
+      /> */}
     </div>
   );
 }
